68c787a5f5948cd41b1f64598dddf21fbc0bb9be
Murilo Portescheller <murilo@blockforce.in>
2021-06-18 15:37:58 -0300

    Add temp clear doctor files on sigint

--- src/pipelines/main.js
+++ src/pipelines/main.js
@@ -1,17 +1,24 @@
 const startDoctor = require('../process/clinic-doctor/start');
+const clearDoctor = require('../process/clinic-doctor/clear');
 
 module.exports = async (params) => {
   const steps = [startDoctor];
   const context = {};
 
   try {
+    process.on('SIGINT', function () {
+      setTimeout(() => {
+        console.log("Caught interrupt signal");
+        clearDoctor(params, context);
+        
+      }, 5000);
+    });
+
     for (let i = 0; i < steps.length; i += 1) {
-      const processExec = await steps[i](params, context);
+      const { key, ...values } = await steps[i](params, context);
 
-      context[steps[i].name] = processExec;
+      context[key] = values;
     }
-
-    await startDoctor(params, context);
   } catch (error) {
     console.error('Error execting MAIN pipeline', error);
 
