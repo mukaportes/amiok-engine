52a5710c760f9401fbecf2c10e60ade345ea37a6
Murilo Portescheller <murilo@blockforce.in>
2021-06-18 15:39:23 -0300

    Add callback to write filepath to ctx on doctor collect

--- src/modules/doctor.js
+++ src/modules/doctor.js
@@ -1,29 +1,18 @@
-const fs = require('fs');
 const ClinicDoctor = require('@nearform/doctor');
 
-const execDoctor = (appStarterPath) => new Promise(async (resolve, reject) => {
+const execDoctor = (appStarterPath, afterCollectCallback) => new Promise(async (resolve, reject) => {
   try {
     const doctor = new ClinicDoctor();
 
     doctor.collect(['node', appStarterPath], function (err, filepath) {
       if (err) throw err
 
-      const reportDataPath = `${filepath}/${filepath}-traceevent`;
-
-      // doctor.visualize(filepath, filepath + '.html', function (err) {
-      //   if (err) throw err
-      // });
-
-      // get file filepath + '.html'
-      fs.readFile(reportDataPath, 'utf8', function (err, content) {
-        if (err) throw err;
-
-        console.log('typeof content', typeof content);
-        // console.log('content', content);
-      });
+      afterCollectCallback({ filePath: `${filepath}/${filepath}-traceevent` });
     });
+
+    resolve('doctor.collect started');
   } catch (error) {
-    console.error('execDoctor error =================', error);
+    console.error('execDoctor error', error);
 
     reject(error);
   }
