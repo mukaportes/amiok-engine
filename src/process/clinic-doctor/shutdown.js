8b2ed8cb11591404aef71bc4d268b6da9b185f08
Murilo Portescheller <murilo@blockforce.in>
2021-06-27 19:41:19 -0300

    Add reuse of apiPID from context at doctor shutdown process

--- src/process/clinic-doctor/shutdown.js
+++ src/process/clinic-doctor/shutdown.js
@@ -1,14 +1,11 @@
-const { netstatByPort } = require('../../modules/cmd');
 const PROCESS_ENUM = require('../../enums/process');
 
 module.exports = async (_, context) => {
   console.info(`Executing process ${PROCESS_ENUM.DOCTOR_SHUTDOWN}`);
   try {
-    const { config: { port } } = context[PROCESS_ENUM.SCRIPT_PREPARE];
+    const { apiPid } = context[PROCESS_ENUM.INFO_API_PID];
 
-    const portProcess = await netstatByPort(port);
-
-    process.kill(portProcess.pid, 'SIGINT');
+    process.kill(apiPid, 'SIGINT');
 
     return { key: PROCESS_ENUM.DOCTOR_SHUTDOWN };
   } catch (error) {
