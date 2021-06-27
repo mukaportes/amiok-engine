18a59772539a571e5af1794a007e4166d1f61cd8
Murilo Portescheller <murilo@blockforce.in>
2021-06-27 19:27:45 -0300

    Add info apiPID process

---
+++ src/process/info/api-pid.js
@@ -0,0 +1,18 @@
+const PROCESS_ENUM = require('../../enums/process');
+const { netstatByPort } = require('../../modules/cmd');
+
+module.exports = async ({ }, context) => {
+  console.info(`Executing process ${PROCESS_ENUM.INFO_API_PID}`);
+  try {
+    const { config } = context[PROCESS_ENUM.SCRIPT_PREPARE];
+    const subProcess = await netstatByPort(config.port);
+
+    if (!subProcess) throw 'No process running in the port given';
+
+    return { key: PROCESS_ENUM.INFO_API_PID, apiPid: subProcess.pid };
+  } catch (error) {
+    console.error(`Error executing ${PROCESS_ENUM.INFO_API_PID} process`, error);
+
+    throw error;
+  }
+};
