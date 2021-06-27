51fafae4309a32129beff929ba1744e3e18b0494
Murilo Portescheller <murilo@blockforce.in>
2021-06-27 19:28:03 -0300

    Add full pipeline

---
+++ src/pipelines/full.js
@@ -0,0 +1,42 @@
+const startDoctor = require('../process/clinic-doctor/start');
+const prepareTestScripts = require('../process/test-script/prepare');
+const executeTestScripts = require('../process/test-script/execute');
+const analyzeDoctor = require('../process/clinic-doctor/analyze');
+const clearDoctor = require('../process/clinic-doctor/clear');
+const shutdownDoctor = require('../process/clinic-doctor/shutdown');
+const getInfoApiPid = require('../process/info/api-pid');
+
+module.exports = async (params) => {
+  const context = {};
+  const steps = [
+    prepareTestScripts,
+    startDoctor,
+    getInfoApiPid,
+    executeTestScripts,
+    shutdownDoctor,
+  ];
+
+  params.collectCallback = (filePath) => {
+    const updatedParams = { ...params, filePath };
+
+    analyzeDoctor(updatedParams, context)
+      .then(() => clearDoctor(updatedParams, context))
+      .catch((error) => {
+        console.error('Error at collectCallback()', error);
+
+        throw error;
+      });
+  };
+
+  try {
+    for (let i = 0; i < steps.length; i += 1) {
+      const { key, ...values } = await steps[i](params, context);
+
+      context[key] = values;
+    }
+  } catch (error) {
+    console.error('Error execting MAIN pipeline', error);
+    
+    process.kill(process.pid, 1);
+  }
+};
