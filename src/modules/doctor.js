1f185e322db909a0fece4122ca207709afd1bacd
Murilo Portescheller <murilo@blockforce.in>
2021-06-27 16:58:35 -0300

    Add getAnalysisFile method to doctor module

--- src/modules/doctor.js
+++ src/modules/doctor.js
@@ -1,16 +1,21 @@
+const fs = require('fs');
+const pumpify = require('pumpify');
+const Stringify = require('streaming-json-stringify')
+const streamTemplate = require('stream-template')
+const ProcessStatDecoder = require('@nearform/doctor/format/process-stat-decoder');
 const ClinicDoctor = require('@nearform/doctor');
+const { netstatByPort } = require('../modules/cmd');
+const { getFileContent } = require('../modules/file');
 
 const execDoctor = (appStarterPath, afterCollectCallback) => new Promise(async (resolve, reject) => {
   try {
     const doctor = new ClinicDoctor();
 
     doctor.collect(['node', appStarterPath], function (err, filepath) {
-      if (err) throw err
-
-      afterCollectCallback({ filePath: `${filepath}/${filepath}-traceevent` });
+      afterCollectCallback({ filePath: filepath });
     });
 
-    resolve('doctor.collect started');
+    resolve(doctor.killCollectEmitter);
   } catch (error) {
     console.error('execDoctor error', error);
 
@@ -18,6 +23,51 @@ const execDoctor = (appStarterPath, afterCollectCallback) => new Promise(async (
   }
 });
 
+const getAnalysisFile = (port, pid = null) => new Promise(async (resolve, reject) => {
+  try {
+    let doctorProcessPid;
+
+    if (!pid) {
+      ({ pid: doctorProcessPid } = await netstatByPort(port));
+    } else {
+      doctorProcessPid = pid;
+    }
+
+    const path = `${process.cwd()}/${doctorProcessPid}.clinic-doctor/${doctorProcessPid}.clinic-doctor-processstat`;
+
+    const processStatReader = pumpify.obj(
+      fs.createReadStream(path),
+      new ProcessStatDecoder()
+    );
+
+    const processStatStringify = pumpify(
+      processStatReader,
+      new Stringify({
+        seperator: ',\n',
+        stringifier: JSON.stringify
+      })
+    );
+
+    const processStatStream = streamTemplate`${processStatStringify}`;
+    let processstat = '';
+
+    processStatStream.on('data', (data) => {
+      processstat += Buffer.from(data).toString();
+    });
+
+    processStatStream.on('end', () => {
+      resolve(processstat);
+    });
+  } catch (error) {
+    console.error('Error retrieving analysis file', error);
+
+    reject(error);
+  }
+});
+
+// const getAverage
+
 module.exports = {
   execDoctor,
+  getAnalysisFile,
 }
