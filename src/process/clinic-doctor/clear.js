8d48b3bb379caeabbce986272bddf73a0984eb4f
Murilo Portescheller <murilo@blockforce.in>
2021-06-27 19:27:24 -0300

    Add doctor clear process

--- src/process/clinic-doctor/clear.js
+++ src/process/clinic-doctor/clear.js
@@ -1,18 +1,21 @@
 const { removeFolder } = require('../../modules/file');
 const PROCESS_ENUM = require('../../enums/process');
 
-module.exports = async (_, context) => {
+module.exports = async ({ filePath }, context) => {
   console.info(`Executing process ${PROCESS_ENUM.DOCTOR_CLEAR}; CTX -->`, context);
 
   try {
-    const { filePath } = context[PROCESS_ENUM.DOCTOR_START];
-    const [parentFolder] = filePath.split('/');
+    console.log('filePath', filePath);
+    let path = filePath || context[PROCESS_ENUM.DOCTOR_START].filePath;
+    const [parentFolder] = path.split('/');
 
-    await removeFolder(parentFolder);
+    console.log('`${process.cwd}/${parentFolder}`', `${process.cwd()}/${parentFolder}`);
+    await removeFolder(`${process.cwd()}/${parentFolder}`);
 
-    return { key: PROCESS_ENUM.DOCTOR_CLEAR };
+    // return { key: PROCESS_ENUM.DOCTOR_CLEAR };
+    process.kill(process.pid, 0);
   } catch (error) {
-    console.error('error executing start server process', error);
+    console.error(`Error executing ${PROCESS_ENUM.DOCTOR_CLEAR} process`, error);
 
     throw error;
   }
