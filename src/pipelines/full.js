b6c05c4d26caaace5c34bc63fd1637bea897712f
mukaportes <mport.dev@gmail.com>
2022-05-10 00:46:53 -0300

    Add new test processes to full pipeline

--- src/pipelines/full.js
+++ src/pipelines/full.js
@@ -1,3 +1,4 @@
+const PROCESS_ENUM = require('../enums/process');
 const startDoctor = require('../process/clinic-doctor/start');
 const prepareTestScripts = require('../process/test-script/prepare');
 const executeTestScripts = require('../process/test-script/execute');
@@ -5,21 +6,29 @@ const analyzeStats = require('../process/stats/analyze');
 const clearDoctor = require('../process/clinic-doctor/clear');
 const shutdownDoctor = require('../process/clinic-doctor/shutdown');
 const getInfoApiPid = require('../process/info/api-pid');
+const prepareStorage = require('../process/storage/prepare');
+const setupTest = require('../process/storage/setup-test');
+const storeTest = require('../process/storage/store-test');
 
 module.exports = async (params) => {
   const context = {};
   const steps = [
+    prepareStorage,
     prepareTestScripts,
     startDoctor,
     getInfoApiPid,
+    setupTest,
     executeTestScripts,
+    storeTest,
     shutdownDoctor,
   ];
 
   params.collectCallback = (filePath) => {
     const updatedParams = { ...params, filePath };
+    const { storage: { storeResourceStats } } = context[PROCESS_ENUM.STORAGE_PREPARE];
 
     analyzeStats(updatedParams, context)
+      .then(({ results }) => storeResourceStats(context[PROCESS_ENUM.STORAGE_TEST_SETUP].test.id, results))
       .then(() => clearDoctor(updatedParams, context))
       .catch((error) => {
         console.error('Error at collectCallback()', error);
