b5b583e81e241267bf5e11291f19c0cebad8987c
mukaportes <mport.dev@gmail.com>
2022-06-05 00:52:11 -0300

    Add init methods to lib entrypoint

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const fullPipeline = require('./src/pipelines/full');
+const { startAmiok, stopAmiok } = require('./src/modules/init');
 
 module.exports = {
   fullPipeline,
+  init: {
+    startAmiok,
+    stopAmiok,
+  },
 };
