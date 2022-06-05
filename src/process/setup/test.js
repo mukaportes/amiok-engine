36d72ba5403ce610085c6b0722f887f3ea48ddf5
mukaportes <mport.dev@gmail.com>
2022-06-05 00:57:25 -0300

    Ad id generation to init strat amiok

--- src/process/setup/test.js
+++ src/process/setup/test.js
@@ -1,16 +1,21 @@
 const crypto = require('crypto');
 const PROCESS_ENUM = require('../../enums/process');
+const { getFirstFileFromFolder } = require('../../modules/file');
+const { setCurrentTestId } = require('../../modules/stats');
 
 /**
- * 
+ *
  * @returns {StorageSetupTestContext}
  */
 module.exports = async () => {
-  console.info(`Executing process ${PROCESS_ENUM.TEST_SETUP}`);
+  console.info(`Executing process ${PROCESS_ENUM.SETUP_TEST}`);
 
-  const test = {
-    id: crypto.randomUUID(),
-  };
+  // get file from _amiok folder
+  const fileName = await getFirstFileFromFolder(`${process.cwd()}/_amiokstats`);
+  const [id] = fileName.split('.stat');
+  const test = { id };
 
-  return { key: PROCESS_ENUM.TEST_SETUP, test };
+  setCurrentTestId(id);
+
+  return { key: PROCESS_ENUM.SETUP_TEST, test };
 };
